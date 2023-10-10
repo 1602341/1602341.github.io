19059e96882e10e35ddddb14da9a94797d2a03d8
1602341 <147089725+1602341@users.noreply.github.com>
2023-10-10 12:17:13 -0600

    started array project

--- array-project/sketch.js
+++ array-project/sketch.js
@@ -1,6 +1,6 @@
 // Array Project
 // Natalie Woo
-// Date
+// October 19. 2023
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -8,8 +8,16 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 600, WEBGL);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  sphere(20);
+  pop();
 }
