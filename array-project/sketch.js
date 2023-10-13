b25ec29e8241657569791545a663945b4ce9e326
1602341 <147089725+1602341@users.noreply.github.com>
2023-10-13 12:16:19 -0600

    changed project idea and started creating a function to spawn petals around the ball

--- array-project/sketch.js
+++ array-project/sketch.js
@@ -5,13 +5,44 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+//let theSphere = [];
+//let state = "startMode"
+
+let x = 0;
+let y = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   createCanvas(800, 600, WEBGL);
   background(0);
-  let x = 0;
-  let y = 0;
+  angleMode(DEGREES)
+}
+
+function keyPressed() {
+  if (keyCode === 37)  {
+
+  }
+}
+
+function moveSphere() {
+  //while state === "gameMode"
+  // if keyPressed() {
+
+  // }
+
+}
+function spawnPetals() {
+  if keyPressed() {
+    thePetal();
+  }
+}
+
+function thePetal() {
+  fill(255);
+  arc(100, 100, width/4, height/2, PI, TWO_PI);
+  x += 10
+  y += 10
+  z += 5
 }
 
 function theSphere() {
@@ -22,9 +53,10 @@ function theSphere() {
   rotateY(frameCount * 0.01);
   sphere(20);
   pop();
-  while 
+  //moveSphere()
 }
 
 function draw() {
   theSphere();
+  thePetal();
 }
