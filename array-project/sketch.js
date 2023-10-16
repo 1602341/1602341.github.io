ddb65c3969f0b339f66d5e16e9fc6a8328cd5ffc
1602341 <147089725+1602341@users.noreply.github.com>
2023-10-16 15:10:45 -0600

    debugged parts of code

--- array-project/sketch.js
+++ array-project/sketch.js
@@ -9,65 +9,57 @@
 //let petalArray = [];
 
 let sphereArray = [];
+let x = 0;
+let y = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   createCanvas(800, 600, WEBGL);
   background(0);
   angleMode(DEGREES)
+  spawnSpheres()
 }
 
 function keyPressed() {
-  if (keyCode === 37)  {
-
-  }
+  let dir = atan2(y - height / 2, x - width / 2);
+  let someSphere = spawnSpheres();
+    someSphere.x = (x += cos(dir + 90) * 5);
+    someSphere.y = (y += sin(dir + 90) * 5);
+    sphereArray.push(someSphere); 
+  
 }
-
-function moveSphere() {
-  //while state === "gameMode"
-  // if keyPressed() {
-
-  // }
-
-}
-// function spawnPetals() {
-//   if keyPressed() {
-//     thePetal();
-//   }
-// }
-
 
 function spawnSpheres() {
   let sphere = {
-    x: z * cos(angle),
-    y: z * sin(angle),
+    x: width/2,
+    y: height/2,
     color: color(random(255), random(255), random(255)),
     dx: 5,
     dy: 5,
   }
-  
   sphereArray.push(sphere);
 }
-function drawSpiral() {
-  let dir = atan2(y - height / 2, x - width / 2);
-  x += cos(dir + 90) * dx;
-  y += sin(dir + 90) * dy;
-}
+// function drawSpiral() {
+//   let dir = atan2(y - height / 2, x - width / 2);
+//   x += cos(dir + 90) * dx;
+//   y += sin(dir + 90) * dy;
+// }
 
 
 function draw() {
   for (let theSphere of sphereArray) {
-    fill(theSphere.color);
-    translate(0, 0, 0);
-    push();
-    rotateZ(frameCount * 0.01);
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
-    sphere(20);
-    pop();
-    fill(theSphere.color);
-    translate(width/2, height/2);
-    drawSpiral();
-    
+    if (keyPressed(keyCode === 37)) {
+      fill(theSphere.color);
+      translate(0, 0, 0);
+      push();
+      rotateZ(frameCount * 0.01);
+      rotateX(frameCount * 0.01);
+      rotateY(frameCount * 0.01);
+      sphere(20);
+      pop();
+      fill(theSphere.color);
+      translate(width/2, height/2);
+    //  drawSpiral();
+    }
   }
 }
