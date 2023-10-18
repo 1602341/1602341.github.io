c0270aa6b84f2a0cc724767385ac1f7563f0d7dc
1602341 <147089725+1602341@users.noreply.github.com>
2023-10-18 07:56:26 -0600

    Fixed major problems with the codes function. Still need to fix the size of the sphere.

--- array-project/sketch.js
+++ array-project/sketch.js
@@ -19,31 +19,37 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   createCanvas(800, 600, WEBGL);
   background(0);
-  angleMode(DEGREES)
-  spawnSpheres();
+  angleMode(DEGREES);
+  let dir = atan2(y - height / 2, x - width / 2);
+  let someSphere = spawnSpheres();
+  someSphere.x = (x + cos(dir + 90) * 5);
+  someSphere.y = (y + sin(dir + 90) * 5);
+  sphereArray.push(someSphere); 
 }
 
-function keyTyped() {
-  //let dir = atan2(y - height / 2, x - width / 2);
-  if (keyCode === 37) {
-    let someSphere = spawnSpheres();
-    // someSphere.x = (x + cos(dir + 90) * 5);
-    // someSphere.y = (y + sin(dir + 90) * 5);
-    sphereArray.push(someSphere); 
+// function keyTyped() {
+//   //let dir = atan2(y - height / 2, x - width / 2);
+//   if (keyCode === 37) {
+//     let someSphere = spawnSpheres();
+//     // someSphere.x = (x + cos(dir + 90) * 5);
+//     // someSphere.y = (y + sin(dir + 90) * 5);
+//     sphereArray.push(someSphere); 
       
-  }
-}
+//   }
+// }
 
 function spawnSpheres() {
   let sphere = {
-    x: width/2,
-    y: height/2,
+    x: width/4,
+    y: height/4,
     color: color(random(255), random(255), random(255)),
     dx: 5,
     dy: 5,
     grow: 1,
     growStep: 0.002, 
     offset: 1,
+    radius: 5,
+    middle: width/2,
   };
 return sphere;
 }
@@ -57,33 +63,41 @@ step = step + .0001;
 
 function draw() {
   for (let theSphere of sphereArray) {
-    middle = width/2;
+    //growStep = 0.0002;
+    //offset = 1;
+    //middle = width/2;
     step = step + 1;
     fill(theSphere.color);
     translate(width/2, height/2);
-    grow = grow + growStep;
+    for (let i = 0; i < 1400; i = i+2) {
+      grow = grow + theSphere.growStep;
+      theSphere.offset = theSphere.offset + 0.01;
+      radius = 1 + (i/2.25);
+      sphereRadius = width/2 + (i/2000);
+      growRadius = 0;
+      growRadius = growRadius + 0.02;
+      radius = radius + growRadius;
+      theSphere.x = theSphere.middle/2 + cos((i) - theSphere.offset) * radius;
+      theSphere.y = theSphere.middle/2 - sin((i) - theSphere.offset) * radius;
+      push();
+      translate (theSphere.x, theSphere.y);
+      fill(theSphere.color);
+      //rotate((-20*cos(i+step)) + abs(12*sin(i+step)));
+      rotateZ(frameCount * 0.1);
+      rotateX(frameCount * 0.1);
+      rotateY(frameCount * 0.1);
+      sphere(sphereRadius);
+      pop();
+      if (mouseIsPressed){
+        theSphere.color = color(random(255), random(255), random(255));
+      }
+    }
   }
-  if (grow > 5 ) { 
-    growStep = -0.002;
-  }
-  if (grow < 0.5 ) { 
-    growStep = 0.002;
-  }
-  for (let i = 0; i < 1400; i=i+2) {
-    offset = offset + 0.0001;
-    radius = 2 + (i/2.25);
-    circleRadius = 45 + (i/30);
-    growRadius = 0;
-    growRadius = growRadius + 0.02;
-    radius = radius + growRadius;
-    sphere.x = middle + cos((i)-offset) * radius;
-    sphere.y = middle - sin((i)-(offset)) * radius;
-    push();
-    translate (xCircle, yCircle);
-    fill(theSphere.color);
-    rotate((-20*cos(i+step)) + abs(12*sin(i+step)));
-    sphere(radius);
-    pop();
-  }
+//   if (grow > 5 ) { 
+//     growStep = -0.002;
+//   }
+//   if (grow < 0.5 ) { 
+//     growStep = 0.002;
+//   }
 }
 
