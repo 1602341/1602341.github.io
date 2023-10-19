c187539ccdf816deca1c8cfda572a8d0fdbcf093
1602341 <147089725+1602341@users.noreply.github.com>
2023-10-18 20:01:42 -0600

    Made final changes and removed dead code. Commented descriptions.

--- array-project/sketch.js
+++ array-project/sketch.js
@@ -8,31 +8,30 @@
 //Reference for the spiral:
 //https://editor.p5js.org/ejuo/sketches/7m5U6m2g-
 
+//Reference for 3D is WEBGL
+//https://p5js.org/examples/3d-geometries.html
+
+//sets up the array and initializes one value
 let sphereArray = [];
-let x = 0;
-let y = 0;
 let growRadius;
-//let step;
-//let grow;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //imports a extension that allows 3D shapes
   createCanvas(800, 600, WEBGL);
   background(0);
   angleMode(DEGREES);
+  //pushes the initial array
   let someSphere = spawnSpheres();
   sphereArray.push(someSphere); 
 }
 
+//function to attach object notaions to
 function spawnSpheres() {
   let sphere = {
     x: width/4,
     y: height/4,
     color: color(random(255), random(255), random(255)),
-    //dx: 5,
-    //dy: 5,
-    grow: 1,
-    growStep: 0.002, 
     offset: 1,
     radius: 5,
     middle: width/2,
@@ -40,15 +39,13 @@ function spawnSpheres() {
 return sphere;
 }
 
-//step = step + .0001;
-
 function draw() {
+  //pulls values from the array
   for (let theSphere of sphereArray) {
-    //step = step + 1;
-    fill(theSphere.color);
+    //sets position for the initial sphere
     translate(width/2, height/2);
+    //sets up the spiral of the spheres
     for (let i = 0; i < 1400; i = i+2) {
-      theSphere.grow = theSphere.grow + theSphere.growStep;
       theSphere.offset = theSphere.offset + 0.01;
       theSphere.radius = 1 + (i/2.25);
       sphereRadius = width/50 + (i/20);
@@ -58,15 +55,16 @@ function draw() {
       theSphere.x = theSphere.middle/4 + cos((i) - theSphere.offset) * theSphere.radius;
       theSphere.y = theSphere.middle/4 - sin((i) - theSphere.offset) * theSphere.radius;
       push();
+      //decides new placement of the spheres
       translate (theSphere.x - width/2, theSphere.y - width/2);
+      //sets up the lighting and texture of the spheres
       normalMaterial()
       specularMaterial(theSphere.color);
-      rotateZ(frameCount * 0.1);
-      rotateX(frameCount * 0.1);
-      rotateY(frameCount * 0.1);
-      pointLight(250, 250, 250, mouseX - width/2, mouseY - width/2, 100);
+      pointLight(247, 230, 166, mouseX - width/2, mouseY - width/2, 100);
+      //spawns the spheres
       sphere(sphereRadius);
       pop();
+      //picks a new random colour once the mouse is pressed
       if (mouseIsPressed){
         theSphere.color = color(random(255), random(255), random(255));
       }
