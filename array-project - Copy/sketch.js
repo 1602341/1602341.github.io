9875566862488bdb6c70c7b2d07c456201d8d1e6
1602341 <147089725+1602341@users.noreply.github.com>
2023-10-29 10:42:57 -0600

    the copy of my array project

---
+++ array-project - Copy/sketch.js
@@ -0,0 +1,90 @@
+// Array Project
+// Natalie Woo
+// October 19, 2023
+//
+// Extra for Experts:
+// - The use of WEBGL to create 3D objects and change textures and lighting
+
+//Reference for the spiral:
+//https://editor.p5js.org/ejuo/sketches/7m5U6m2g-
+
+//Reference for 3D is WEBGL
+//https://p5js.org/examples/3d-geometries.html
+
+//Description:
+// - Used spheres to create a 3D spiral
+// - When the mouse is clicked the spiral will change colours
+// - When the Up and Down arrow keys are pressed the lighting changes
+
+//sets up the array and initializes one value
+let sphereArray = [];
+let growRadius; 
+let light = 500;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //imports a extension that allows 3D shapes
+  createCanvas(800, 600, WEBGL);
+  background(0);
+  angleMode(DEGREES);
+  //pushes the initial array
+  let someSphere = spawnSpheres();
+  sphereArray.push(someSphere); 
+}
+
+//function to attach object notaions to
+function spawnSpheres() {
+  let sphere = {
+    x: width/4,
+    y: height/4,
+    color: color(random(255), random(255), random(255)),
+    offset: 1,
+    radius: 5,
+    middle: width/2,
+  };
+return sphere;
+}
+
+//changes the birghtness of the light when the arrow keys are pressed
+function keyPressed() {
+  if (keyIsDown(UP_ARROW) && light <= 1000) {
+    light = light + 20
+  }
+  else if (keyIsDown(DOWN_ARROW) && light >= 0) {
+    light = light - 20
+  }
+}
+function draw() {
+  keyPressed()
+  //pulls values from the array
+  for (let theSphere of sphereArray) {
+    //sets position for the initial sphere
+    translate(width/2, height/2);
+    //sets up the spiral of the spheres
+    for (let i = 0; i < 1400; i = i+2) {
+      theSphere.offset = theSphere.offset + 0.01;
+      theSphere.radius = 1 + (i/2.25);
+      sphereRadius = width/50 + (i/20);
+      growRadius = 1;
+      growRadius = growRadius + 0.02;
+      theSphere.radius = theSphere.radius + growRadius;
+      theSphere.x = theSphere.middle/4 + cos((i) - theSphere.offset) * theSphere.radius;
+      theSphere.y = theSphere.middle/4 - sin((i) - theSphere.offset) * theSphere.radius;
+      push();
+      //decides new placement of the spheres
+      translate (theSphere.x - width/2, theSphere.y - width/2);
+      //sets up the lighting and texture of the spheres
+      normalMaterial()
+      specularMaterial(theSphere.color);
+      pointLight(247, 230, 166, mouseX - width/2, mouseY - width/2, light);
+      //spawns the spheres
+      sphere(sphereRadius);
+      pop();
+      //picks a new random colour once the mouse is pressed
+      if (mouseIsPressed){
+        theSphere.color = color(random(255), random(255), random(255));
+      }
+    }
+  }
+}
+
