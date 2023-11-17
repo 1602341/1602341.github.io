6553b31c7870dd17544c4a5616a1f5c4e403abae
1602341 <147089725+1602341@users.noreply.github.com>
2023-11-17 07:39:32 -0600

    finished implementing miniMax function. Fixed most bugs but there are still some problems occurring.

--- 2d-grid-project/sketch.js
+++ 2d-grid-project/sketch.js
@@ -16,19 +16,16 @@
 let i;
 let n;
 let grid;
-let section;
 let cellSize;
-let sectionSize;
-let playerX;
-let playerO;
-let theTurn;
-let turnX = 0;
-let turnO = 1;
-let ai = "aiturnX";
-let human = "humanturnO";
-let state = "twoPlayer"
-const GRID_SIZE = 9;
-const SECTION_SIZE = 3;
+let turn;
+let human;
+let ai;
+let player = human;
+// let ai = "X";
+// let human = "O";
+let state = "startScreen"
+const GRID_SIZE = 3;
+//const SECTION_SIZE = 3;
 
 
 function preload() {
@@ -45,66 +42,184 @@ function setup() {
   else {
     cellSize = height/GRID_SIZE; 
   }
-  if (height > width) {
-    sectionSize = width/SECTION_SIZE;
-  }
-  else {
-    sectionSize = height/SECTION_SIZE; 
-  }
+  // if (height > width) {
+  //   sectionSize = width/SECTION_SIZE;
+  // }
+  // else {
+  //   sectionSize = height/SECTION_SIZE; 
+  //}
   grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  bestMove();
 }
 
-function miniMax() {
-  if (state === "ai") {
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++ ) {
-        if (grid[j][i] === '') {
-          
+function find3(a, b, c) {
+  return a === b && b === c && a!== '';
+}
+
+// function findWinner() {
+//   let winner = theWinner;
+// }
+
+function checkWinner() {
+  if (state === "singlePlayer") {
+    let winner = null;
+    for (let y = 0; y < GRID_SIZE; y++) {
+      //horizontal
+      if (find3(grid[y][0], grid[y][1], grid[y][2])) {
+        winner = grid[y][0];
+      }
+      //vertical
+      else if (find3(grid[0][y], grid[1][y], grid[2][y])) {
+        winner = grid[0][y];
+      }
+    }
+      //diagonal
+    if (find3(grid[0][0], grid[1][1], grid[2][2])) {
+      winner = grid[0][0];
+    }
+    if (find3(grid[2][0], grid[1][1], grid[0][2])) {
+      winner = grid[2][0];
+    }
+    let openSpots = 0;
+    for (let y = 0; y < GRID_SIZE; y++) {
+      for (let x = 0; x < GRID_SIZE; x++) {
+        if (grid[y][x] == '') {
+          openSpots++;
         }
       }
     }
+    if (winner === null && openSpots === 0) {
+      return ("tie");
+    }
+    else {
+      return winner;
+    }
+  }
+}
+
+function mousePressed() {
+  if (state === "startScreen") {
+    let singleClicked = isInRect(mouseX, mouseY, height/2, 
+    height/2 + 200, width/3, width/3 + 300);
+    if (singleClicked) {
+      state = "singlePlayer";
+      turn = human
+    }
+  }
+}
+
+function start() {
+  if (state === "startScreen") {
+    textSize(width/10);
+    fill('black');
+    text('Tic Tac Toe', width/4, height/3);
+    rect(width/3, height/2, 300, 200);
+    textSize(width/20)
+    fill('white');
+    text('Single Player', width/2.7, height/1.6);
+  }
+  else if ((state === "singlePlayer") || (state === "multiPlayer")) {
+  displayMainGrid();
+  //displaySectionGrid();
   }
 }
 
 function draw() {
   background(220);
-  displayMainGrid();
-  displaySectionGrid();
+  start();
+  checkWinner();
+}
+
+function isInRect(x, y, top, bottom, left, right) {
+  return x >= left && x <= right && y >= top && y <= bottom;
 }
 
 function boxFinder(x, y) {
-  if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) {
-      if (grid[y][x] === turnX) {
-        grid[y][x] = turnO;
+  if (state === "singlePlayer") {
+        grid[y][x] = ai;
+    if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) {
+      if (grid[y][x] === '') {
+        bestMove();
+        grid[y][x] = ai;
       }
-      else if (grid[y][x] === turnO) {
-        grid[y][x] = turnX;
+      else if (grid[y][x] === ai) {
+        bestMove();
+        grid[y][x] = human;
       }
+    }
   }
 }
 
 function mouseClicked() {
   let y = Math.floor(mouseY/cellSize);
   let x = Math.floor(mouseX/cellSize);
-  let i = Math.floor(mouseY/sectionSize);
-  let n = Math.floor(mouseX/sectionSize);
+  // let i = Math.floor(mouseY/sectionSize);
+  // let n = Math.floor(mouseX/sectionSize);
   boxFinder(x, y)
-  if (state === "twoPlayer") {
-    //theTurn = "turnX";
-    //if (state === "turnX") {
-        state = "turnO"
-    //}
-    //else if (state === "turnO") {
-      //state = "turnX"
-    //}
-    // if (theTurn === "turnX") {
-    //   displayMainGrid()
-    //   theTurn = "turnO";
-    // }
-    // else if (theTurn === "turnO"){
-    //   displayMainGrid()
-    //   theTurn = "turnX";
-    // }
+}
+
+function bestMove() {
+  let bestScore = -Infinity;
+  let move;
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (grid[y][x] === '') {
+        grid[y][x] = ai;
+        let score = miniMax(grid);
+        grid[y][x] = '';
+        if (score > bestScore) {
+          bestScore = score;
+          move = { y, x };
+          grid[move.y][move.x] = ai;
+          player = human;
+        }
+      }
+    }
+  }
+}
+
+let scores = {
+  X: 10,
+  O: -10,
+  tie: 0
+}
+
+function miniMax(grid, depth, maximizing) {
+  let result = checkWinner();
+  if (state === "startScreen") {
+    if (result !== null) {
+      return scores[result];
+    }
+
+    if (maximizing) {
+      let bestScore = -Infinity;
+      for (let y = 0; y < GRID_SIZE; y++) {
+        for (let x = 0; x < GRID_SIZE; x++) {
+          if (grid[y][x] === '') {
+            grid[y][x] = ai;
+            let score = miniMax(grid, depth + 1, false);
+            grid[y][x] = '';
+            bestScore = max(score, bestScore)
+          }
+        }
+      }
+      return bestScore;
+    }
+    else {
+      let bestScore = Infinity;
+      for (let y = 0; y < GRID_SIZE; y++) {
+        for (let x = 0; x < 3; x++) {
+          // Is the spot available?
+          if (grid[y][x] == '') {
+            grid[y][x] = human;
+            let score = miniMax(grid, depth + 1, true);
+            grid[y][x] = '';
+            bestScore = min(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    }
   }
 }
 
@@ -114,56 +229,44 @@ function displayMainGrid() {
     for (let x = 0; x < GRID_SIZE; x++) {
       strokeWeight(5)
       rect(x * cellSize, y * cellSize, cellSize, cellSize);
-      if (state === "turnX") {
-        if (grid[y][x] === turnX) {
-          image(playerO, x * cellSize, y * cellSize, width/9, height/9);
-          //theTurn === "turnO"
-          state = "turnO"
-          //rect(i * cellSize, n * cellSize, cellSize, cellSize);
-          //rect(i * sectionSize, n * sectionSize, sectionSize, sectionSize);
-        }
+      let spot = grid[y][x];
+      if (spot === human) {
+        image(playerX, x * cellSize, y * cellSize, width/3, height/3); 
       }
-    
-      if (state === "turnO") {
-        if (grid[y][x] === turnO) {
-          image(playerX, x * cellSize, y * cellSize, width/9, height/9);
-          state === "turnX"
-          //fill("grey")
-          //rect(0, 0, width/3, height/3);
-        } 
-        //rect(i * sectionSize, n * sectionSize, sectionSize, sectionSize);
-        // else if (grid[y][x] === 0) {
-          //   image(playerO, x * cellSize, y * cellSize, width/9, height/9);
-          // } 
-        }
-    }
-  }
-}
-
-function displaySectionGrid() {
-  for (let y = 0; y < SECTION_SIZE; y++) {
-    for (let x = 0; x < SECTION_SIZE; x++) {
-      fill(0, 0, 30, 20);
-      strokeWeight(10);
-      //rect(x * cellSize, y * cellSize, cellSize, cellSize);
-      rect(x * sectionSize, y * sectionSize, sectionSize, sectionSize);
-      // if (theTurn === "turnX") {
-      //   if (grid[y][x] === 0) {
-      //     theTurn === "turnX"
-      //     fill("grey")
-      //     rect(i * sectionSize, n * sectionSize, sectionSize, sectionSize);
+      else if (spot === ai) {
+        image(playerO, x * cellSize, y * cellSize, width/3, height/3);
+      }
+      // if (turn === "turnX") {
+      //   if (grid[y][x] === "turnX") {
+      //     image(playerO, x * cellSize, y * cellSize, width/9, height/9);
+      //     turn = "turnO"
       //   }
       // }
-      // else if (theTurn === "turnO") {
-      //   if (grid[y][x] === 1) {
-      //     theTurn === "turnO"
-      //     fill("grey")
-      //     rect(i * sectionSize, n * sectionSize, sectionSize, sectionSize);
+      // else if (turn === "turnO") {
+      //   if (grid[y][x] === "turnO") {
+      //     image(playerX, x * cellSize, y * cellSize, width/9, height/9);
+      //     state === "turnX"
       //   } 
       // }
     }
-  }  
-}
+  }
+//   let result = checkWinner();
+//   if (result !== null) {
+//     noLoop();
+//     state = "endScreen";
+//   }
+ }
+
+// function displaySectionGrid() {
+//   //if (state === singlePlayer || state === multiplayer) {
+//     for (let y = 0; y < SECTION_SIZE; y++) {
+//       for (let x = 0; x < SECTION_SIZE; x++) {
+//         fill(0, 0, 30, 20);
+//         strokeWeight(10);
+//         rect(x * sectionSize, y * sectionSize, sectionSize, sectionSize);
+//     }
+//   }  
+// }
 
 function generateEmptyGrid(cols, rows) {
   let randomArray = [];
